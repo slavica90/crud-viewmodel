a967217b7d08b2437893158f9a49ed4ddc5dcc10
slavica90 <slavica90@gmail.com>
2019-01-08 08:08:28 +0100

    testing viemodel structure

---
+++ screen/viewmodel/TableBind.js
@@ -0,0 +1,39 @@
+class TableBind {
+
+   constructor(){
+      this.data = new OneModel()
+   }
+
+   init(id){
+      this.data.read(this, this.onCB) //reading the data (real or fake)
+
+      let _this = this
+
+      depp.require(['tabLoaded' ], function() { 
+         _this.table = new Tabulator("#"+id, {
+            layout:"fitColumns",      //fit columns to width of table
+            columns:[ //Define Table Columns
+               {title:"id", field:"id", visible:false},
+               {title:"Col1", field:"col1", align:"left", width:'50%'},
+               {title:"Col2", field:"col2", align:"left", width:'50%'},
+            ],
+            rowClick:function(e, row){ //trigger 
+               var row = row.getData()
+               console.log('row: ', row);
+               sessionStorage.setItem('row', JSON.stringify(row)); //save object in sessionStorage, to retrive it on the next page(form)
+               window.location.replace('/screen/viewmodel/form');
+            },
+         })//tab
+         depp.done('tabReady')
+      })//depp
+
+   }//()
+
+   onCB (rows, ctx) {
+      depp.require(['tabReady' ], function() { 
+         ctx.table.clearData()
+         ctx.table.setData(rows)
+      })
+   }//()
+
+}//class
---
+++ screen/viewmodel/test/TestBind.js
@@ -0,0 +1,80 @@
+
+class TestBind {
+
+   constructor(){
+      this.form = ''
+      this.row = typeof sessionStorage.getItem('row') !='undefined' && JSON.parse(sessionStorage.getItem('row'))
+      this.data = new OneModel()
+   }
+
+   init(divId){
+      
+      this.form = '#'+divId
+      if( this.row !=null){
+         this.onCB() //run 'onCB' only when there was a click on the row in the table, other way no data needed in the form
+      }
+   }
+
+   add(row) {
+      let validation = this.data.valid(row) //do the validation
+      
+      if(validation=='OK')
+         this.data.add(row)
+      else  {
+         console.log('error', validation)
+      } //else
+   }
+
+   update(row) {
+      let validation = this.data.valid(row) //do the validation
+      
+      if(validation=='OK')
+         this.data.update(row)
+      else  {
+         console.log('error', validation)
+      } //else
+   }
+
+   delete(row) {
+      this.data.delete(row)
+   }
+   
+   onCB() {
+      let _this = this
+
+      $(_this.form).find('input[name="col1"]').val(this.row['col1'])
+      $(_this.form).find('input[name="col2"]').val(this.row['col2'])
+      $(_this.form).find('input[name="id"]').val(this.row['id'])
+   }
+
+   
+   getFields() {
+      let lst = {}
+      //start w/ pk
+      let input = $(this.form+' [name="id"]')
+      lst['id'] = input.val()
+   
+      $(this.form+' input').each(
+         function(index){  
+            let input = $(this)
+            lst[ input.attr('name')] = input.val()
+         }//index
+      )//each
+      console.log('--lst',lst)
+      return lst
+   }//()
+
+
+   clearFields(){
+      sessionStorage.removeItem('row');
+      $(this.form).find('input').val('')
+   }
+
+   countData(){
+      this.data.countData();
+      //console.log("povik:",this.data.countData());
+      //console.log("number", number);
+      //$(this.form).find('input[name="countData"]').val(number);
+      
+   }
+}
